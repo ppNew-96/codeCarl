d833a026fae11da57b02343c45189cee93d4e685
ppNew-96 <n2679450052@163.com>
2023-04-27 20:08:37 +0800

    remove specific element

---
+++ 01Array/02removeEle.cpp
@@ -0,0 +1,31 @@
+// leetcode 27
+
+#include <iostream>
+#include <vector>
+class Solution {
+public:
+  int removeElement(std::vector<int> &nums, int val) {
+    int leftPtr = 0;
+    int rightPtr = nums.size() - 1;
+    while (leftPtr <= rightPtr) {
+      while (leftPtr <= rightPtr && nums[leftPtr] != val)
+        leftPtr++;
+      while (leftPtr <= rightPtr && nums[rightPtr] == val)
+        rightPtr--;
+      if (leftPtr < rightPtr) {
+        nums[leftPtr] = nums[rightPtr];
+        leftPtr++;
+        rightPtr--;
+      }
+    }
+    return leftPtr;
+  }
+};
+
+int main() {
+  std::vector<int> nums = {1};
+  Solution solve;
+  int result = solve.removeElement(nums, 1);
+  std::cout << result << std::endl;
+  return 0;
+}
