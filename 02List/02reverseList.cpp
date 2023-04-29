d611c56da5759a815538a8775a3cc1f090e41889
ppNew-96 <n2679450052@163.com>
2023-04-29 21:52:11 +0800

    reverse list

---
+++ 02List/02reverseList.cpp
@@ -0,0 +1,40 @@
+// leetcode 206
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr){};
+  ListNode(int x, ListNode *ptr = nullptr) : val(x), next(ptr){};
+};
+
+// version: no recursive
+// class Solution {
+// public:
+//   ListNode *reverseList(ListNode *head) {
+//     ListNode *cur = head;
+//     ListNode *pre = nullptr;
+//     while (cur != nullptr) {
+//       ListNode *temp = cur->next;
+//       cur->next = pre;
+//       pre = cur;
+//       cur = temp;
+//     }
+//     return pre;
+//   }
+// };
+
+// recursive version:
+// sub-problem: reverse and return new head node
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    if (head == nullptr)
+      return nullptr;
+    if (head->next == nullptr)
+      return head;
+    ListNode *last = reverseList(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return last;
+  }
+};
